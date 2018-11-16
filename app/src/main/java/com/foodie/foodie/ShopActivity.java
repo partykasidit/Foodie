128d37c1ccda5adeac169c8d335a7aef299614ce
Kasidit <kasidit.09@hotmail.com>
2018-11-16 20:18:26 +0700

    Succesfully retrive Vendors from Firebase

--- app/src/main/java/com/foodie/foodie/ShopActivity.java
+++ app/src/main/java/com/foodie/foodie/ShopActivity.java
@@ -1,20 +1,21 @@
 package com.foodie.foodie;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class ShopActivity extends AppCompatActivity {
 
@@ -26,42 +27,29 @@ public class ShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
-        ArrayList<Vendor> vendors = getDataFromDatabase();
-
-        mVendorList = findViewById(R.id.rv_vendor_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mVendorList.setLayoutManager(layoutManager);
-        mVendorList.setHasFixedSize(true);
-        mListAdapter = new VendorListAdapter(vendors);
-        mVendorList.setAdapter(mListAdapter);
-
-    }
-
-    private ArrayList<Vendor> getDataFromDatabase() {
-
-        final ArrayList<Vendor> vendors = new ArrayList<>();
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("Vendors").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        db.collection("Vendors").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        Map<String,Object> data = document.getData();
-                        Log.d("Foodie-SA",data.get("TH_name").toString());
-                        //why arrayList not adding element?
-                        vendors.add(new Vendor(data.get("TH_name").toString(),data.get("EN_name").toString(),R.drawable.ic_launcher_background));
-                    }
-                } else {
-                    Log.w("Foodie-SA", "Error getting documents.", task.getException());
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                ArrayList<Vendor> vendors = new ArrayList<>();
+                if (e != null) {
+                    Log.w("Foodie-SA", "listen:error", e);
+                    return;
                 }
+
+                for (DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()) {
+                    Map<String,Object> data = dc.getDocument().getData();
+                    vendors.add(new Vendor(data.get("TH_name").toString(),data.get("EN_name").toString(),R.drawable.ic_launcher_background));
+
+                }
+                mVendorList = findViewById(R.id.rv_vendor_list);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                mVendorList.setLayoutManager(layoutManager);
+                mVendorList.setHasFixedSize(true);
+                mListAdapter = new VendorListAdapter(vendors);
+                mVendorList.setAdapter(mListAdapter);
             }
         });
-
-        Log.d("Foodie-SA",String.valueOf(vendors.size()));
-
-        return vendors;
-
     }
 
 }
