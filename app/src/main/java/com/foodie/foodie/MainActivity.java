924959a2243c67c57c04e0d667ba20f53ae5cfcf
kan1657 <kan1657@gmail.com>
2018-12-17 22:18:11 +0700

    added LoginActivity

--- app/src/main/java/com/foodie/foodie/MainActivity.java
+++ app/src/main/java/com/foodie/foodie/MainActivity.java
@@ -3,6 +3,7 @@ package com.foodie.foodie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         Button shopButton = findViewById(R.id.shop_button);
 
+        Log.d("Main","Test Main");
+
+
     }
 
     public void onShopButtonClicked(View view) {
         Intent intent = new Intent(MainActivity.this,ShopActivity.class);
+        startActivity(intent);
+    }
+
+    public void onLoginButtonClicked(View view){
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
     }
 }
