9019e119fcfdc311ead4d1065fca5d15586800c9
klod <sagun.phetkaew@gmail.com>
2018-12-19 14:49:31 +0700

    loginActivity_error

---
+++ foodievendor/src/main/java/com/foodie/foodievendor/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.foodie.foodievendor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+// decide here whether to navigate to Login or Main Activity
+
+        SharedPreferences pref = getSharedPreferences("ActivityPREF", Context.MODE_PRIVATE);
+        if (pref.getBoolean("activity_executed", false)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        Log.d("Splash","Test Splash");
+    }
+
+}
